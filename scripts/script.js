30845ec9c8802198230e0e8e6766bfb60bd49f1d
Simranjyot Singh Gill <simranjyot1991@gmail.com>
2017-06-29 01:19:49 -0400

    faster and smooth loading of page

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
+    $('<img/>').attr('src', '../images/background-1.jpg').on('load', function() {
+        initLoadAnimations();
+    });
+});
+
+$(window).resize(function(event) {
+    var greeting_top = $("#hello").offset().top;
+    var name_top = greeting_top + $("#hello").height();
+    $("#name").css('top', name_top);
+
+    padding_right = 50;
+    var width = $("#name").find('span:first').width() + padding_right;
+    var location = $(window).width() - width;
+    $("#name").css('left', location);
+});
+
+function initLoadAnimations(){
     $("#hello").animate({
         opacity: 1},
-        3000, function() {
+        2000, function() {
             $(this).css('height', $("#inner-hello").height());
             var greeting_top = $(this).offset().top;
             var name_top = greeting_top + $(this).height();
@@ -17,7 +34,9 @@ $(document).ready(function() {
                     width = width + padding_right;
                     $("#name").width(width);
                     var location = $(window).width() - width;
-                    $("#name").animate({'left': location}, 1000);
+                    $("#name").animate({'left': location}, 1000, function(){
+                        $(".scroll").css('animation-play-state', 'running');
+                    });
                 }
             }).tiPause(400)
             .tiType("Simranjyot ")
@@ -26,15 +45,6 @@ $(document).ready(function() {
             .tiPause(400)
             .tiType("Gill");
     });
-});
-
-$(window).resize(function(event) {
-    var greeting_top = $("#hello").offset().top;
-    var name_top = greeting_top + $("#hello").height();
-    $("#name").css('top', name_top);
-
-    padding_right = 50;
-    var width = $("#name").find('span:first').width() + padding_right;
-    var location = $(window).width() - width;
-    $("#name").css('left', location);
-});
+    $(".cover").animate({opacity:1}, 500);
+    $(".nav-bar").animate({opacity:1}, 500);
+}
